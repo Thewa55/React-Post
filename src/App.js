f3330b8c57f97b59508457355e03ab6d10ea81b1
Kenny Lam <kinwai.lam730@gmail.com>
2020-05-14 16:10:24 -0400

    Added starter code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Allpost from './components/Allpost'
+import Postform from './components/Postform'
+
+function App() {
+  return (
+    <div className="App">
+      <Allpost />
+      <Postform />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Postform/index.js
@@ -0,0 +1,37 @@
+import React, {useRef} from 'react';
+import {useDispatch} from 'react-redux'
+
+function Postform(){
+  const dispatch = useDispatch()
+  const title = useRef()
+  const post = useRef()
+
+  function handleFormSubmit(event){
+    event.preventDefault();
+    const postTitle = title.current.value
+    const postContent = post.current.value
+    const data = {
+      id: new Date(),
+      postTitle,
+      postContent
+    }
+    dispatch({type: 'ADD_POST', data})
+    title.current.value =""
+    post.current.value = ""
+  }
+  
+  return(
+    <div className="Postform">
+      <h1>Create Post</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input required type="text" placeholder="Enter Post Title" ref={title} />
+        <br /> <br />
+        <textarea required rows="5" cols="28" placeholder="Enter Post" ref={post}/>
+        <br /><br />
+        <button>Post</button>
+      </form>
+    </div>
+  )
+}
+
+export default Postform;
