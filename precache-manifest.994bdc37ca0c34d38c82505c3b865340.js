2f65724d868712392338635af30d1a6fefe75afa
Kenny Lam <kinwai.lam730@gmail.com>
2020-05-14 23:09:47 -0400

    Updates

---
+++ precache-manifest.994bdc37ca0c34d38c82505c3b865340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db8dffe6398426b5d0faace1dc4dae7",
+    "url": "/React-Post/index.html"
+  },
+  {
+    "revision": "ac17f3661914256ec614",
+    "url": "/React-Post/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc699af4384e6ae66b79",
+    "url": "/React-Post/static/js/2.da2b8af4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Post/static/js/2.da2b8af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac17f3661914256ec614",
+    "url": "/React-Post/static/js/main.ea9241cf.chunk.js"
+  },
+  {
+    "revision": "ffc3ad7883313b4297d7",
+    "url": "/React-Post/static/js/runtime-main.2f7ed01b.js"
+  }
+]);
