1f1c41f929267df7794f97bbfe4c496e6c9e5fc8
Kenny Lam <kinwai.lam730@gmail.com>
2020-07-10 21:50:09 -0400

    Able to edit value with new value

--- src/components/Editcomponent/index.js
+++ src/components/Editcomponent/index.js
@@ -1,27 +1,29 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import {useDispatch} from 'react-redux'
 
 function Editcomponent(props) {
   const dispatch = useDispatch()
   const title = useRef()
   const post = useRef()
+  const [newUserTitle, setNewUserTitle] = useState(props.post.postTitle)
+  const [newPost, setNewPost] = useState(props.post.postContent)
 
   function handleEdit(event){
     event.preventDefault();
-    const newTitle = title.current.value
-    const newContent = post.current.value
+    const newTitle = newUserTitle
+    const newContent = newPost
     const data = {
       newTitle,
       newContent
     }
     dispatch({type: 'UPDATE_POST', id: props.post.id, data: data})
   }
-  
+
   return(
     <div>
       <form onSubmit={handleEdit}>
-        <input required type="text" ref={title} defaultValue={props.post.postTitle} ></input>
-        <textarea required typeof="text" ref={post} defaultValue={props.post.postContent}></textarea>
+        <input required type="text" ref={title} defaultValue={props.post.postTitle} onChange={e => setNewUserTitle(e.target.value)}></input>
+        <textarea required typeof="text" ref={post} defaultValue={props.post.postContent} onChange={e => setNewPost(e.target.value)}></textarea>
         <button>Update</button>
       </form>
     </div>
